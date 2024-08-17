637932ca3b8e37eb602ccb442512fbda28ed6afb
Rafael Maia <rafaelmaia3002@gmail.com>
2024-08-16 21:34:51 -0300

    Defining main carousel styling and speed

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
+    // menu
+    $(document).ready(function(){
 
-    $('#nav-toggle').click(function(e){
-        e.preventDefault();
-        $(this).toggleClass('active');
-        $('.header-collapse').toggleClass('active');
+        $('#nav-toggle').click(function(e){
+            e.preventDefault();
+            $(this).toggleClass('active');
+            $('.header-collapse').toggleClass('active');
+        });
     });
-});
 
-// Carousel Principal
-$('.owl-carousel').owlCarousel({
-    items:1,
-    lazyLoad:true,
-    loop:true,
-    margin: 0,
-    nav: true,
-    navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>']
+    // Carousel Principal
+    $('#carousel_principal').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin: 0,
+        nav: true,
+        navSpeed: 1000,
+        navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+        dots: true,
+        dotsSpeed: 1000,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10
+    });
 });
