c2f9d4d583c6dcd30ace4b36b0a1a0f8415db6c8
Rafael Maia <rafaelmaia3002@gmail.com>
2024-09-05 07:40:01 -0300

    marking HTML for section 'Testemunhas' and activating carousel

--- scripts/script.js
+++ scripts/script.js
@@ -24,4 +24,19 @@ $(document).ready(function(){
         autoplaySpeed: 1000,
         responsiveRefreshRate: 10
     });
+    // Carousel Testemunhas
+    $('#carousel_testemunhas').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin: 0,
+        nav: true,
+        navSpeed: 1000,
+        navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+        dots: true,
+        dotsSpeed: 1000,
+        // autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10
+    });
 });
