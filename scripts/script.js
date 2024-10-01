eb223665ff7b1e1441d6f4de99489c8b7345738f
Rafael Maia <rafaelmaia3002@gmail.com>
2024-09-30 21:11:46 -0300

    Implement jQuery functions to open and close modals

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
+    // dialog
+    $('.dialog-close').click(function(){
+        $('.dialog-body').fadeOut('200', function(){
+            $('.dialog').fadeOut('200');
+        });
+    });
+
+    $('.dialog-open').click(function(e){
+        e.preventDefault();
+
+        var target = $(this).attr('href');
+        $('.dialog').fadeIn('200', function(){
+            $(target).fadeIn('200');
+        });
+    });
+
     // menu / nav toggle
     $('#nav-toggle').click(function(e){
         e.preventDefault();
