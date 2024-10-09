46bab43d9ce13c5226c23385adc6a162990a475c
Rafael Maia <rafaelmaia3002@gmail.com>
2024-10-09 15:53:16 -0300

    feat: added carousel to portfolio modal with styles and functionality

--- scripts/script.js
+++ scripts/script.js
@@ -117,6 +117,22 @@ $(document).ready(function(){
         }
     });
 
+    // Carousel Portfolio
+    $('#carousel_portfolio').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin: 0,
+        nav: true,
+        navSpeed: 1000,
+        navText: ['<i class="fa-solid fa-chevron-left"></i>','<i class="fa-solid fa-chevron-right"></i>'],
+        dots: false,
+        dotsSpeed: 1000,
+        // autoplay: true,
+        autoplaySpeed: 1000,
+        responsiveRefreshRate: 10
+    });
+
     // Portfolio
     $('.portfolio-nav li a').click(function(e){
         e.preventDefault();
